d4b887ee4a2b8caf7e7e27e416302fb8e4b9d769
cc01748 <cc01748@surrey.ac.uk>
2022-04-01 14:25:50 +0100

    Fixed variable name mistake (maybe?) my_profaile

--- backend/users/profile/urls.py
+++ backend/users/profile/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from users.profile.views.Profile import SelfUserProfile, UserProfile
 
 urlpatterns = [
-    path('', UserProfile.as_view(), name="my_profaile"),
+    path('', UserProfile.as_view(), name="my_profile"),
     path('me', SelfUserProfile.as_view(), name="my_profile"),
     # path('login', Login.as_view(), name="user_login"),
     # path('refresh_token', RefreshToken.as_view(), name="user_refresh_token"),
