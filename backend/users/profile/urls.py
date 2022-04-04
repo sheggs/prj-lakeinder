73ab2e3eb532b55b4277d0a98f7adec5e76074ca
cc01748 <cc01748@surrey.ac.uk>
2022-04-04 15:55:11 +0100

    changed path names to more distinct values

--- backend/users/profile/urls.py
+++ backend/users/profile/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from users.profile.views.Profile import SelfUserProfile, UserProfile
 
 urlpatterns = [
-    path('', UserProfile.as_view(), name="my_profile"),
-    path('me', SelfUserProfile.as_view(), name="my_profile"),
+    path('', UserProfile.as_view(), name="user_profile"),
+    path('me', SelfUserProfile.as_view(), name="self_profile"),
     # path('login', Login.as_view(), name="user_login"),
     # path('refresh_token', RefreshToken.as_view(), name="user_refresh_token"),
 ]
